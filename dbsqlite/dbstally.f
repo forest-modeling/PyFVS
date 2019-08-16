9233362908c71055b79d7b60a7e0f93880d2ac44
mshettles521 <mshettles521@90b89b48-9230-0de4-5065-812dd12741b8>
2019-08-16 22:50:04 +0000

    Added a final row to the FVS_Regen_Tally table for "All" species, where the TPA values are the summation of all species and the average height of the best trees is a weighted average.
    git-svn-id: https://svn.code.sf.net/p/open-fvs/code/branches/FMSCdev@2756 90b89b48-9230-0de4-5065-812dd12741b8

--- dbsqlite/dbstally.f
+++ dbsqlite/dbstally.f
@@ -1,5 +1,6 @@
       SUBROUTINE DBSTALLY(PRBSTK,NTALLY,SPECCD,SUMTPA,SUMPCT,BESTTPA,
-     & BESTPCT,AVEHT,PASTPA,PASPCT,IYEAR)
+     & BESTPCT,AVEHT,PASTPA,PASPCT,IYEAR,WTAVEHT,TOT,TOTTTPAA,
+     & TOTTTPAB,TOTTTPA3)
       IMPLICIT NONE
 C----------
 C DBSQLITE $Id: dbsstats.f 2620 2019-03-08 18:22:51Z nickcrookston $
@@ -19,12 +20,12 @@ C
 COMMONS
 C
       INTEGER ColNumber,iret,NTALLY,SUMTPA1,SUMPCT1,BESTTPA1
-      INTEGER PASTPA1,PASPCT1,BESTPCT1,IYEAR
-      REAL PRBSTK,AVEHT
+      INTEGER PASTPA1,PASPCT1,BESTPCT1,IYEAR,TOT
+      REAL PRBSTK,AVEHT,WTAVEHT,TOTTTPAA,TOTTTPAB,TOTTTPA3
       REAL SUMTPA,SUMPCT,BESTTPA,PASTPA,PASPCT,BESTPCT
       DOUBLE PRECISION PRBSTK1,AVEHT1
       CHARACTER*2000 SQLStmtStr
-      CHARACTER*2 SPECCD      
+      CHARACTER*3 SPECCD      
 C
 COMMONS END
 
@@ -83,17 +84,32 @@ C
 C
 C     ASSIGN REAL VALUES TO DOUBLE PRECISION VARS
 C        
-        AVEHT1=AVEHT
+
         PRBSTK1=PRBSTK
+        IF(TOT.EQ.1)THEN
+        AVEHT1=AVEHT
+        ELSE
+        AVEHT1=WTAVEHT
+        ENDIF
 C
 C     ASSIGN INTEGER VALUES TO REAL VARS
 C 
+        
+        IF(TOT.EQ.1)THEN
         SUMTPA1=NINT(SUMTPA)
-        SUMPCT1=NINT(SUMPCT)
         BESTTPA1=NINT(BESTTPA)
         PASTPA1=NINT(PASTPA)
         PASPCT1=NINT(PASPCT)
         BESTPCT1=NINT(BESTPCT)
+        SUMPCT1=NINT(SUMPCT) 
+        ELSE
+        SUMTPA1=NINT(TOTTTPAA)
+        BESTTPA1=NINT(TOTTTPAB)
+        PASTPA1=NINT(TOTTTPA3)
+        PASPCT1=100
+        BESTPCT1=100
+        SUMPCT1=100
+        ENDIF                                               
                           
         ColNumber=1
         iRet = fsql3_bind_int(IoutDBref,ColNumber,IYEAR)
@@ -138,7 +154,7 @@ C
      -              'StandID char(26) null,'//
      -              'Year int null,'//
      -              'TallyNumber int null,'//
-     -              'Species Char(2) null,'//
+     -              'Species Char(3) null,'//
      -              'TpaAll int null,'//
      -              'PctOfTotalAll int null,'//
      -              'TpaBest int null,'//
@@ -170,16 +186,29 @@ C
 C
 C     ASSIGN REAL VALUES TO DOUBLE PRECISION VARS
 C        
+        IF(TOT.EQ.1)THEN
         AVEHT1=AVEHT
+        ELSE
+        AVEHT1=WTAVEHT
+        ENDIF
 C
 C     ASSIGN INTEGER VALUES TO REAL VARS
-C 
+C        
+        IF(TOT.EQ.1)THEN
         SUMTPA1=NINT(SUMTPA)
-        SUMPCT1=NINT(SUMPCT)
         BESTTPA1=NINT(BESTTPA)
         PASTPA1=NINT(PASTPA)
         PASPCT1=NINT(PASPCT)
         BESTPCT1=NINT(BESTPCT)
+        SUMPCT1=NINT(SUMPCT) 
+        ELSE
+        SUMTPA1=NINT(TOTTTPAA)
+        BESTTPA1=NINT(TOTTTPAB)
+        PASTPA1=NINT(TOTTTPA3)
+        PASPCT1=100
+        BESTPCT1=100
+        SUMPCT1=100
+        ENDIF              
                           
         ColNumber=1
         iRet = fsql3_bind_int(IoutDBref,ColNumber,IYEAR)
