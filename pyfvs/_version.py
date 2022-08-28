8cc110bae850cfa6f4acbbff5eb90f9dfeeb2403
Tod Haren <tod.haren@gmail.com>
2022-08-28 01:38:32 -0700

    Update version before PyScaffold setup

--- pyfvs/_version.py
+++ pyfvs/_version.py
@@ -1,6 +1,6 @@
 # This file is automatically updated during packaging.
 # The version is extracted from the current Git tag using setup.py version.
 
-__version__ = '0.2.0.dev0'
+__version__ = '0.2.1.dev0'
 __git_describe__ = ''
-__git_commit__ = '6f731ecd-dirty'
+__git_commit__ = 'e2cc94fb-dirty'
