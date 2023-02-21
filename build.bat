ce84302cd4e945100af8dc78ce6f485511e3816a
Tod Haren <tod.haren@gmail.com>
2023-02-21 01:26:35 -0800

    Update Meson build scripts

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
-@REM conda activate pyfvs38
+@REM conda activate pyfvs310
 @REM set variants=pn,wc,so,op,oc
 @REM set variants=pn,wc,so,op,oc,ec,ca
-set variants=pn
+@REM set variants=pn
 
-python setup.py develop --no-deps -- -DFVS_VARIANTS=%variants%
+@REM check for rtools40/ucrt64/bin/gfortran.exe
+
+python -m pip install --no-build-isolation -v -e . --config-settings editable-verbose=true
